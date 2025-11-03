02cf9a78b8e72169ccb961508981f9b1594d7096
Anudeepthi45 <110282266+Anudeepthi45@users.noreply.github.com>
2025-11-03 23:31:48 +0530

    Result

---
+++ Result/result.py
@@ -0,0 +1,8 @@
+The Logistic Regression model achieved excellent performance in predicting whether a user would click on an online advertisement, with an overall:
+  - Accuracy --> 98%
+  - Precision of --> 98.9%
+  - Recall of --> 97%
+  - F1-score of --> 98%
+The confusion matrix indicated very few misclassifications, showing that the model effectively distinguished between users who clicked and those who did not. 
+Key influencing factors included **Age**, **Daily Internet Usage**, and **Daily Time Spent on Site**, suggesting that users who are younger and spend more time online are more likely to engage with advertisements.
+Overall, the model demonstrated high reliability and can be used to improve targeted advertising strategies.
